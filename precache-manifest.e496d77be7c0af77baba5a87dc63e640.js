e8c9e7c869b5546e12a14b4755f5e8db28f07afa
techSavvySaahil <saahil.madaan@gmail.com>
2019-02-04 21:52:47 +0530

    Updates

---
+++ precache-manifest.e496d77be7c0af77baba5a87dc63e640.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8a09cabf110dfb48caa",
+    "url": "/video_capture/static/js/runtime~main.f8a09cab.js"
+  },
+  {
+    "revision": "5ffb5cf1de036d0b4ef4",
+    "url": "/video_capture/static/js/main.5ffb5cf1.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/video_capture/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "5ffb5cf1de036d0b4ef4",
+    "url": "/video_capture/static/css/main.03ef58a0.chunk.css"
+  },
+  {
+    "revision": "e1c4f6d4770bbe99c5c1d4b0144853ba",
+    "url": "/video_capture/index.html"
+  }
+];
