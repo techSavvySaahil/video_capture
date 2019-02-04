102dd6ded999d2e75dfd62670c55f543242b7403
techSavvySaahil <saahil.madaan@gmail.com>
2019-02-04 14:48:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Recorder from './recorder'
+import './styles/App.css';
+import Recorder from './components/Recorder/Recorder'
 
 class App extends Component {
   state = {
     vidList: [],
     currentlyPlaying: ""
   }
-  updateList = (media)=> {
-    this.setState((prevState)=> ({
-      vidList: [...prevState.vidList, media]
-    }))
+
+  updateList = (media, index)=> {
+    let vidList = [...this.state.vidList];
+    if(index) {
+      vidList.splice(index,1);
+    }
+    else {
+      vidList.push(media);
+    }
+    this.setState({ vidList });
+    window.localStorage.setItem("vidList", JSON.stringify(vidList));
   }
+
   showVid = (src)=> {
     this.setState({
       currentlyPlaying: src
@@ -22,14 +29,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div style={{display:"flex"}}>
-          <video id="record" autoPlay></video>
-          <video id="play" src={this.state.currentlyPlaying} autoPlay controls></video>
+      <header>
+      Capture & Play
+      </header>
+        <div className="recorder-wrap">
+          <div><p>Live Video</p>
+            <video id="record" autoPlay></video>
+          </div>
+          <div><p>Recorded Video</p>
+            <video id="play" src={this.state.currentlyPlaying} autoPlay controls></video>
+          </div>
         </div>
         <Recorder updateList={this.updateList} />
-        <div style={{display:"flex", flexWrap:"wrap"}}>
-        {this.state.vidList && this.state.vidList.map(media=> (
-            <video style={{width:"100px", height:"100px"}} key={media.thumbnail} src={media.thumbnail} onClick={()=>this.showVid(media.src)}></video>
+        <hr />
+        <p>Recorded Video List</p>
+        <div className="list-wrap">
+        {this.state.vidList && this.state.vidList.map((media, index)=> (
+          <div key={media.thumbnail} className="thumbnail">
+            <video src={media.thumbnail} onClick={()=>this.showVid(media.src)}></video>
+            <button onClick={()=>{this.updateList(media,index)}}>Delete</button>
+          </div>
         ))}
         </div>
       </div>
--- src/components/Recorder/Recorder.js
+++ src/components/Recorder/Recorder.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-
+import './Record.css';
 class Recorder extends Component{
+  state = {
+    recording: false
+  }
 
   startVid = ()=> {
     const constraints = {
@@ -10,10 +13,23 @@ class Recorder extends Component{
 
     window.video = document.querySelector('video#record');
 
-    navigator.mediaDevices.getUserMedia(constraints).
-      then((stream) => {
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
         window.video.srcObject = stream;
         window.stream = stream;
+      })
+      .catch(err=>{
+        switch(err.code) {
+          case 0:
+            alert("We need your camera and microphone permissions");
+            break;
+          case 8:
+            alert("You don't have a camera/microphone attached to your system");
+            break;
+          default:
+            alert("Couldn't get your media devices");
+        }
+        console.log(err);
       });
   }
   stopVid = ()=> {
@@ -29,6 +45,7 @@ class Recorder extends Component{
     }
   }
   startRecording=()=> {
+    this.setState({ recording: true });
     var options = {mimeType: 'video/webm;codecs=vp9', bitsPerSecond: 100000};
     window.recordedBlobs = [];
     try {
@@ -44,6 +61,7 @@ class Recorder extends Component{
           options = 'video/mp4';
           window.mediaRecorder = new MediaRecorder(window.stream, options);
         } catch (e2) {
+          this.setState({ recording: false });
           alert('Not supported by this browser.');
           console.error('Exception while creating MediaRecorder:', e2);
           return;
@@ -55,12 +73,15 @@ class Recorder extends Component{
   }
 
   stopRecording=()=> {
+    this.setState({ recording: false });
     window.mediaRecorder.stop();
     let srcVid = new Blob(window.recordedBlobs, {type: 'video/webm'});
     let thumbVid = new Blob(
       [window.recordedBlobs[0],window.recordedBlobs[1]],
       {type: 'video/webm'}
       );
+    srcVid = new File([srcVid], Date.now(), {type: 'video/webm', lastModified: Date.now()});
+    thumbVid = new File([thumbVid], Date.now(), {type: 'video/webm', lastModified: Date.now()});
     let src = window.URL.createObjectURL(srcVid);
     let thumbnail = window.URL.createObjectURL(thumbVid);
     this.props.updateList({src, thumbnail});
@@ -71,10 +92,10 @@ class Recorder extends Component{
 
   render() {
     return(
-      <div>
-        <button onClick={this.startRecording}>Record</button>
-        <button onClick={this.stopRecording}>Stop</button>
-        </div>
+      <div className="record-btn-wrap">
+        <button onClick={this.startRecording} disabled={this.state.recording}>Record</button>
+        <button onClick={this.stopRecording} disabled={!this.state.recording}>Stop</button>
+      </div>
     )
   }
 }
